7349aa0022f56e0c9faa53e96573918dc6567d48
d1vr <ranaa90100@gmail.com>
2022-08-02 15:11:37 +0300

    init mhg.

---
+++ assets/js/language.js
@@ -0,0 +1,31 @@
+class Translate{
+    constructor(){
+        document.getElementById("arabic").addEventListener("click",()=>{
+            this.translate("arabic");
+        });
+
+        document.getElementById("eng").addEventListener("click",()=>{
+            this.translate("eng");
+        });
+        this.translate(localStorage.getItem("Laugauge"));
+    }
+    translate(language){
+        if(language == "arabic"){
+            
+            document.getElementById("lang1").innerHTML="موظفو ومتدرب MHG IT";
+            document.getElementById("lang2").innerHTML="نجعل التكنولوجيا في المتناول! وكل شيء سهل";
+            document.getElementById("lang3").innerHTML="احجز الان";
+
+        }
+        else if(language == "eng"){
+
+            document.getElementById("lang1").innerHTML="MHG IT Staff & Trainee";
+            document.getElementById("lang2").innerHTML="We make technology accessible! And Everything is easy";
+            document.getElementById("lang3").innerHTML="Book now";
+
+        }
+        localStorage.setItem("Laugauge",language)
+
+    }
+}
+onload=new Translate();
