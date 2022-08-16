f29c375087df832489891559064fdcf64cb30ebd
d1vr <ranaa90100@gmail.com>
2022-08-16 15:24:45 +0300

    add php

--- contact.php
+++ contact.php
@@ -61,3 +61,33 @@ if($_POST) {
 } else {
     echo '<p>Something went wrong</p>';
 }
+<?php
+$servername = "localhost";
+$username = "rana_2_user";
+$password = "Rana1234@1234";
+$dbname = "rana_2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?> 
